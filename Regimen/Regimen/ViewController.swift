e2a7cd23356439cf68e3587c5dd8431a31fb9e4c
ace5584 <aexace666@gmail.com>
2021-07-29 21:07:25 +1000

    added first table view on homepage with placeholders

--- Regimen/Regimen/ViewController.swift
+++ Regimen/Regimen/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    
+    
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    let count = 20
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        heightConstraint.constant = CGFloat(Double(count) * 44.5)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel!.text = "My Name is Alex"
+        return cell!
     }
 
 
