18e8970504f18004dd3f35ad0a27535d4cfa9250
ace5584 <aexace666@gmail.com>
2021-08-19 18:58:20 +1000

    Created new project that would be used to test out settings view controller

---
+++ SettingsTeset/SettingsTeset/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsTeset
+//
+//  Created by Alex Lai on 19/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
