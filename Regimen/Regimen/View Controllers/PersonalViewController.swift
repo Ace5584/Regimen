c2dcc70157afe0cad13ac04a6481e62ab420ce1b
ace5584 <aexace666@gmail.com>
2021-08-26 09:09:59 +1000

    Regrouped all code into folders and added profile picture capabilities on personal page

---
+++ Regimen/Regimen/View Controllers/PersonalViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PersonalViewController.swift
+//  Regimen
+//
+//  Created by Alex Lai on 26/8/21.
+//
+
+import UIKit
+
+class PersonalViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var profilePicture: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profilePicture.layer.cornerRadius = profilePicture.bounds.width / 2
+        profilePicture.layer.masksToBounds = true
+        profilePicture.layer.borderWidth = 3
+        profilePicture.layer.borderColor = UIColor.systemTeal.cgColor
+        
+        
+    }
+    
+    @IBAction func btnPicture(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        profilePicture.image = image
+        dismiss(animated: true)
+    }
+    
+}
