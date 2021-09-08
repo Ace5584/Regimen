3db38991fe3d68c8189822f1f3a45022f9638d68
Alex <aexace666@gmail.com>
2021-09-08 12:40:37 +1000

    Edit and delete works now and add page works

--- Android Regimen/app/src/main/java/com/example/androidregimen/AddTaskPage.java
+++ Android Regimen/app/src/main/java/com/example/androidregimen/AddTaskPage.java
@@ -2,13 +2,105 @@ package com.example.androidregimen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddTaskPage extends AppCompatActivity {
+    EditText editTitle;
+    EditText editTime;
+    CheckBox checkBoxEveryday;
+    ImageButton btnComplete;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    LinearLayout timeLayout;
+
+    ArrayList<String> gridList = new ArrayList<String>();
+    public static final String LIST = "list";
+
+    Boolean TimeBased = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task_page);
+
+        loadData();
+
+        radioGroup = findViewById(R.id.taskRadioGroup);
+        editTitle = findViewById(R.id.taskTitle);
+        editTime = findViewById(R.id.editTime);
+        checkBoxEveryday = findViewById(R.id.checkboxEveryday);
+        btnComplete = findViewById(R.id.btnFinished);
+
+        btnComplete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TimeBased){
+                    if (editTitle.getText().toString() != "" && editTime.getText().toString() != ""){
+                        String check = "false";
+                        if(checkBoxEveryday.isChecked())
+                            check = "true";
+                        createItem(editTitle.getText().toString(), "Time Based", check,
+                                editTime.getText().toString());
+                        saveData();
+                        finish();
+                    }
+                }
+                else {
+                    String check = "false";
+                    if(checkBoxEveryday.isChecked())
+                        check = "true";
+                    createItem(editTitle.getText().toString(), "Time Based", check, "0");
+                    saveData();
+
+                    finish();
+                }
+            }
+        });
+
+    }
+
+    public void checkButton(View v){
+        int radioId = radioGroup.getCheckedRadioButtonId();
+
+        radioButton = findViewById(radioId);
+
+        Toast.makeText(this, "Selected Radio Button: " + radioButton.getText(), Toast.LENGTH_SHORT).show();
+
+        timeLayout = findViewById(R.id.timeLayout);
+
+        if(radioButton.getId() == R.id.radioTimeBased) {
+            timeLayout.setVisibility(View.VISIBLE);
+            TimeBased = true;
+        }
+        else {
+            timeLayout.setVisibility(View.INVISIBLE);
+            TimeBased = false;
+        }
+    }
+
+    private void saveData(){
+        TinyDB tinydb = new TinyDB(this);
+        tinydb.putListString(LIST, gridList);
+    }
+
+    private void loadData(){
+        TinyDB tinydb = new TinyDB(this);
+        gridList = tinydb.getListString(LIST);
+    }
+
+    public void createItem(String title, String type, String everyday, String time){
+        gridList.add(title + "," + "0," + type + "," + everyday + "," + time);
+        // Title | Percentage | Type | Everyday | Time
     }
 }
--- Android Regimen/app/src/main/java/com/example/androidregimen/HomePage.java
+++ Android Regimen/app/src/main/java/com/example/androidregimen/HomePage.java
@@ -1,21 +1,29 @@
 package com.example.androidregimen;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
@@ -25,17 +33,60 @@ import java.util.Collections;
 import java.io.*;
 import java.util.Set;
 
+class Processor {
+    String item;
+  // Title | Percentage | Type | Everyday | Time
+     String Title;
+     String Percentage;
+     String Type;
+     String Everyday;
+     String Time;
+
+    public Processor(String s) {
+        item = s;
+        String[] spt = item.split("[,]");
+        Title = spt[0];
+        Percentage = spt[1];
+        Type = spt[2];
+        Everyday = spt[3];
+        Time = spt[4];
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getPercentage() {
+        return Percentage;
+    }
+
+    public String getEveryday() {
+        return Everyday;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public String getType() {
+        return Type;
+    }
+}
+
 public class HomePage extends AppCompatActivity {
 
     GridView gridView;
     Button btnDelete;
     Button btnEdit;
     ImageButton btnAdd;
-    ArrayList<String> fruitNames = new ArrayList<>(Arrays.asList("Apple", "Orange", "Strawberry", "Watermelon", "Kiwi", "Banana"));
-//    int[] fruitImages = {R.drawable.apple, R.drawable.oranges, R.drawable.strawberry,
-//            R.drawable.watermelon, R.drawable.kiwi, R.drawable.banana};
+    ArrayList<String> delList = new ArrayList<String>();
 
-    public static final String SHARED_PREFS = "sharedPrefs";
+    boolean editMode = false;
+
     public static final String LIST = "list";
 
     public ArrayList<String> gridList = new ArrayList<String>();
@@ -44,6 +95,16 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        loadData();
+                        refreshGrid();
+                    }
+                });
 
         loadData();
 
@@ -61,43 +122,70 @@ public class HomePage extends AppCompatActivity {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), DetailsPage.class);
-                intent.putExtra("name", fruitNames.get(position));
-//                intent.putExtra("image", fruitImages[position]);
-                startActivity(intent);
+                if(editMode){
+                    ImageView check = view.findViewById(R.id.selectCheckbox);
+                    if (delList.contains(String.valueOf(position))){
+                        check.setVisibility(View.INVISIBLE);
+                        delList.remove(String.valueOf(position));
+                    }
+                    else {
+                        check.setVisibility(View.VISIBLE);
+
+                        delList.add(String.valueOf(position));
+                    }
+                }
+                else {
+
+                    Intent intent = new Intent(getApplicationContext(), DetailsPage.class);
+                    Processor temp = new Processor(gridList.get(position));
+                    intent.putExtra("name", temp.getTitle());
+                    startActivity(intent);
+                }
             }
         });
 
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gridList.add("Hello");
-                saveData();
-                System.out.println(gridList);
+                if(editMode) {
+                    for(int i = 0; i < gridView.getChildCount(); i++) {
+                        ImageView check = gridView.getChildAt(i).findViewById(R.id.selectCheckbox);
+                        check.setVisibility(View.INVISIBLE);
+                    }
+                    btnEdit.setText("Edit");
+                    editMode = false;
+
+                }
+                else{
+                    btnEdit.setText("Done");
+                    editMode = true;
+                }
             }
         });
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gridList.remove(0);
-                saveData();
-                System.out.println(gridList);
-                CustomAdapter customAdapter = new CustomAdapter();
-                gridView.setAdapter(customAdapter);
-                setGridViewHeightBasedOnChildren(gridView, 2);
+                if(delList.size() != 0 && editMode){
+                    for(String i : delList) {
+                        gridList.remove(Integer.parseInt(i));
+                    }
+                    refreshGrid();
+                    saveData();
+                    delList = new ArrayList<String>();
+                }
             }
         });
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveData();
                 Intent intent = new Intent(v.getContext(), AddTaskPage.class);
-                startActivity(intent);
+                someActivityResultLauncher.launch(intent);
             }
         });
     }
-
     private class CustomAdapter extends BaseAdapter{
 
         @Override
@@ -121,8 +209,11 @@ public class HomePage extends AppCompatActivity {
 
             TextView name = view1.findViewById(R.id.fruits);
 //            ImageView image = view1.findViewById(R.id.images);
+            if (gridList.size() != 0) {
 
-            name.setText(gridList.get(position));
+                Processor temp = new Processor(gridList.get(position));
+                name.setText(temp.getTitle());
+            }
 //            image.setImageResource(fruitImages[position]);
             return view1;
         }
@@ -142,7 +233,7 @@ public class HomePage extends AppCompatActivity {
             View listItem = gridViewAdapter.getView(0, null, gridView);
             listItem.measure(0, 0);
             totalHeight = listItem.getMeasuredHeight();
-        
+
             float x;
             if (items > noOfColumns) {
                 x = items / noOfColumns;
@@ -174,10 +265,25 @@ public class HomePage extends AppCompatActivity {
     private void saveData(){
         TinyDB tinydb = new TinyDB(this);
         tinydb.putListString(LIST, gridList);
+        System.out.println(gridList);
     }
 
     private void loadData(){
         TinyDB tinydb = new TinyDB(this);
         gridList = tinydb.getListString(LIST);
+        System.out.println(gridList);
     }
+
+    public void refreshGrid(){
+        CustomAdapter customAdapter = new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+        setGridViewHeightBasedOnChildren(gridView, 2);
+    }
+
+    public void createItem(String title, String type, String everyday, String time){
+        gridList.add(title + "," + "0," + type + "," + everyday + "," + time);
+        // Title | Percentage | Type | Everyday | Time
+    }
+
+
 }
